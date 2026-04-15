f54c5a2611adcf110b7e85ecd98a62ce8a9ddf6d
priyanshu-twer <priyanshu.kumar@thoughtworks.com>
2026-04-15 16:16:38 +0530

    chore(release): v1.0.3

--- .release-it.js
+++ .release-it.js
@@ -9,7 +9,7 @@ module.exports = {
   },
 
   github: {
-    release: true,
+    release: false,
     releaseName: 'v${version}',
     releaseNotes: token => {
       return token.changelog || 'Initial release or no changes detected.';
